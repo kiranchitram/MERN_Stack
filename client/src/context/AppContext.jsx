25e09281a5ded808ee96555b469808a4e4e9ed67
kiranchitram <165831369+kiranchitram@users.noreply.github.com>
2025-09-04 20:43:30 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -17,7 +17,7 @@ export const AppContextProvider =(props)=>{
 
     const getAuthState = async ()=>{
         try {
-            const {data} = await axios.get(backendUrl + '/api/auth/is-auth')
+            const {data} = await axios.get(backendUrl + '/api/auth/is-auth',{ withCredentials: true });
             if(data.success){
                 setIsLoggedin(true)
                 getUserData()
