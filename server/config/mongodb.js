bad66a04dcab49d74e589410a65fbd20f6c91342
kiranchitram <165831369+kiranchitram@users.noreply.github.com>
2025-09-05 12:42:10 +0530

    Update mongodb.js

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 const connectDB=async()=>{
     mongoose.connection.on('connected',()=>console.log("Database Connected"));
-    await mongoose.connect(`${process.env.MONGODB_URI}/MERN-auth`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/MERN-Stack`)
 };
 export default connectDB; 
