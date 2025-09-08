c5125c03ca65f442603f682d51b70ee87f6cc254
kiranchitram <165831369+kiranchitram@users.noreply.github.com>
2025-09-08 17:37:14 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,9 +2,9 @@ import userModel from "../models/userModel.js";
 
 export const getUserData = async (req,res)=>{
     try{
-        //const {userId} = req.query;
+        const {userId} = req.body;
 
-        const user = await userModel.findById(req.params.id);
+        const user = await userModel.findById(userId);
         if(!user){
             return res.json({success: false, message: 'User not Found'});
         }
