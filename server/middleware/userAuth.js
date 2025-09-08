6bda763d827955f27a77e924925cc4e5f289f947
kiranchitram <165831369+kiranchitram@users.noreply.github.com>
2025-09-08 16:42:02 +0530

    Update userAuth.js

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -17,7 +17,7 @@ const userAuth = async (req, res, next) => {
     const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
 
     if (tokenDecode.id) {
-      req.params.id= tokenDecode.id;
+      req.body.userId= tokenDecode.id
       //console.log(tokenDecode.id);
      
     
