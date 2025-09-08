c6650c81f5e4b0b95216ed9b313b3902b87d167b
kiranchitram <165831369+kiranchitram@users.noreply.github.com>
2025-09-08 10:27:16 +0530

    Update userAuth.js

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 
 
 const userAuth = async (req, res, next) => {
-  const token  = req.header("Authorization")?.replace("Bearer","");
+  const token  = req.cookies;
 
   if (!token) {
     return res.status(401).json({
