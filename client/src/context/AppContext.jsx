26b225ec8567fc439b7bf797abff937ade511037
kiranchitram <165831369+kiranchitram@users.noreply.github.com>
2025-09-09 19:58:14 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContent = createContext()
 
 export const AppContextProvider =(props)=>{
 
-   axios.defaults.withCredentials =true ;
+   //axios.defaults.withCredentials =true ;
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const[isLoggedin, setIsLoggedin] = useState(false)
