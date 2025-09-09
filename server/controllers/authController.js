96d2f3422626b1ef5ecde2cb941a79fa79adc6ba
kiranchitram <165831369+kiranchitram@users.noreply.github.com>
2025-09-09 21:21:05 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -30,8 +30,8 @@ export const register = async (req, res)=>{
 
             res.cookie('token', token, 
             {httpOnly: true,
-             secure : process.env.NODE_ENV === "production",
-             sameSite: process.env.NODE_ENV === "production" ? 'strict' : 'none' ,
+             secure : process.env.NODE_ENV === 'production',
+             sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'none' ,
              maxAge: 7 * 24 * 60 * 60 * 1000 
             }); 
         
@@ -75,8 +75,8 @@ export const login =async (req, res)=>{
             
             res.cookie('token', token, 
             { httpOnly: true,
-             secure: process.env.NODE_ENV === "production",
-             sameSite: process.env.NODE_ENV === "production" ? 'strict' : 'none' ,
+             secure: process.env.NODE_ENV === 'production',
+             sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'none' ,
              maxAge: 7 * 24 * 60 * 60 * 1000 
             }); 
             
