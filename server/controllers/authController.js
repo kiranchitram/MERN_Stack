9f0e39002fa9362bea71348b85230852f5259d59
kiranchitram <165831369+kiranchitram@users.noreply.github.com>
2025-09-14 10:56:06 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -31,7 +31,7 @@ export const register = async (req, res)=>{
             res.cookie('token', token, 
             {httpOnly: true,
              secure : true,
-             sameSite: 'none' ,
+             sameSite: 'None' ,
              maxAge: 7 * 24 * 60 * 60 * 1000 
             }); 
         
@@ -76,7 +76,7 @@ export const login =async (req, res)=>{
             res.cookie('token', token, 
             { httpOnly: true,
              secure: true,
-             sameSite: 'none',
+             sameSite: 'None',
              maxAge: 7 * 24 * 60 * 60 * 1000 
             }); 
             
