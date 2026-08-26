11017226dcfff3da50c4d1e60e44d4c375ccecaf
kiranchitram <165831369+kiranchitram@users.noreply.github.com>
2026-08-26 11:19:37 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,7 +12,7 @@ const {userData} = useContext(AppContent)
         <img src={assets.header_img} alt="" className='w-36 h-36 
         rounded-full mb-6'/>
         <h1 className='flex items-center gap-2 text-xl sm:text-3xl 
-        font-medium mb-2'>Hey, {userData ? userData.name : 'Developer'}!
+        font-medium mb-2'>Hey {userData ? userData.name : 'Developer'}!
          <img className='w-8 aspect-square' 
         src={assets.hand_wave} alt="" /></h1>
         <h2 className='text-3xl sm:text-5xl font-semibold mb-4'>Welcome to our App</h2>
